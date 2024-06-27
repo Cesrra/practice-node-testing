d09b9d9fd407a2c2f22dcac861699a56a70069f8
cesrra <c3z4r.rincon@gmail.com>
2024-06-26 19:08:39 -0500

    Add the configuration to nyc to get a full report about tests

--- test/calculator.spec.ts
+++ test/calculator.spec.ts
@@ -3,6 +3,7 @@ import Calculator from "../src/calculator"
 import sinon, { SinonStub } from "sinon"
 
 describe("Test Calculator Class", () => {
+    const  calcu = new Calculator()
     let stub: SinonStub
 
     afterEach(() => {
@@ -11,36 +12,38 @@ describe("Test Calculator Class", () => {
     })
 
     it("Should return Sum", () => {
-        const calcu = new Calculator()
         stub = sinon.stub(calcu, "getRandomValue").returns(8) 
         const result = calcu.add(2,3)
 
         expect(result).to.equal(13)
     })
 
-    it("Should return Substract", () => {
-        const calcu = new Calculator()
+    it("Should return Substract", () => {    
         const result = calcu.substract(2,3)
 
         expect(result).to.equal(-1)
     })
 
     it("Should return Divide", () => {
-        const calcu = new Calculator()
         const result = calcu.divide(6,3)
 
         expect(result).to.equal(2)
     })
 
     it("Should return Multiple", () => {
-        const calcu = new Calculator()
         const result = calcu.multiple(2,3)
 
         expect(result).to.equal(6)
     })
 
     it("Should throu an Error when divide by cero", () => {
-        const calcu = new Calculator()
         expect(() => calcu.divide(2,0)).to.throw("Can not divide by cero!")
+    })
+
+    it("Should return a ramdon number, from 1 to 10", () => {
+        const result = calcu.getRandomValue()
+        expect(result).to.be.a("number")
+        expect(result).to.be.at.least(1)
+        expect(result).to.be.at.most(10)
     })
 })
